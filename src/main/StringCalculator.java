7553bfa09c15f9b464ca1db0a34d9aa7f8eae7cb
Ahtritus <sutirthabharatidas@gmail.com>
2021-07-21 19:28:05 +0530

    calculator works with one and two numbers with , as delimiter

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -1,10 +1,23 @@
 package main;
 
 public class StringCalculator {
-    public static int Add(String text_input) {
-        if (text_input == null || text_input.length() == 0) {
+    public static int Add(String numbers) {
+        //checks if input string numbers is empty
+        if (numbers == null || numbers.length() == 0) {
             return 0;
         }
-        return '\0';
+
+        //splits the input string into a list of numbers
+        //each number is separated by a non digit character
+        String[] split_numbers = numbers.split("\\,");
+
+        //initializes the sum of the numbers
+        int sum = 0;
+
+        //iterates through the list of numbers and calculates the sum
+        for (String number : split_numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 }
--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -13,4 +13,14 @@ public class StringCalculatorTest {
 	public void testEmptyString() {
 		assertEquals("empty string input should return 0", 0, StringCalculator.Add(""));
 	}
+
+    @Test
+    public void testOneNumber() {
+        assertEquals("one number input should return that number", 6, StringCalculator.Add("6"));
+    }
+
+    @Test
+    public void testTwoNumbers() {
+        assertEquals("two numbers input should return sum of those numbers", 5, StringCalculator.Add("3,2"));
+    }
 }
